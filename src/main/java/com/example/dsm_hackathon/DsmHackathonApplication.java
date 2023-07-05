7df2f0b8aac8e4ade673df2c8dcb0777d6a7621d
csh0927 <sung060927@gmail.com>
2023-07-05 09:01:12 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/dsm_hackathon/DsmHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.dsm_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmHackathonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DsmHackathonApplication.class, args);
+    }
+
+}
